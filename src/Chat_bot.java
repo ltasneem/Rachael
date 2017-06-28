19b55c7fb51970416ffcbd24486f0af329c1b7cf
Srikant Kumar Kalaputapu <skalaputapu@gmail.com>
2017-06-28 13:27:23 -0400

    Added names

--- src/Chat_bot.java
+++ src/Chat_bot.java
@@ -1,7 +1,18 @@
-
+/*
+ * Eliza Chat Bot Implementation with 'Sentiniments'
+ * 
+ * Toma Tadros
+ * Srikant Kumar Kalaputapu
+ * Taimur Sohrab
+ * Lubaba Nuzhat Tasneem
+ *  
+ * June 28th, 2017
+ */
 public class Chat_bot {
 
 	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
 		// TODO Auto-generated method stub
 		//ooga booga
 	}
