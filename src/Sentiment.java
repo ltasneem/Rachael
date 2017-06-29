c980406035d4ca4ee11d8aac40e2628fd181197c
Taimur <tsohrab8@gmail.com>
2017-06-29 09:17:52 -0400

    first one

--- src/Sentiment.java
+++ src/Sentiment.java
@@ -1,4 +1,16 @@
+import java.util.*;
 
 public class Sentiment {
-
+	
+	private Set<String> Positive = new HashSet<String>();
+	private Set<String> Negative = new HashSet<String>();
+	private Set<String> Neutral = new HashSet<String>();
+	
+	private HashMap<String,Set<String>> replacementMap = new HashMap<String,Set<String>>();
+	
+	public Sentiment(){
+		
+		
+		
+	}
 }
