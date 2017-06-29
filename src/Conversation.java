e06efac204a7cc69f4d1abf2a02d69f4294bc6f1
Toma Tadros <limbo43@gmail.com>
2017-06-29 09:30:17 -0400

    added some conversation stuff

--- src/Conversation.java
+++ src/Conversation.java
@@ -7,15 +7,15 @@ import java.io.*;
  * */
 
 public class Conversation {
-	
+
 	/*Constructor
 	 * */
-	
+
 	public Conversation()
 	{
 		initialize_directory();
 	}
-	
+
 	/*
 	 * initializes the parent directory
 	 */
@@ -23,95 +23,97 @@ public class Conversation {
 	{
 		String strDirectoy = "Patients";
 		try{
-		    if(!(new File(strDirectoy)).isDirectory())
-		    {
-			    boolean success = (new File(strDirectoy)).mkdir();
-			    if (success) {
-			      System.out.println("Directory: " + strDirectoy + " created");
-			    }    
-	
-		    }
+			if(!(new File(strDirectoy)).isDirectory())
+			{
+				boolean success = (new File(strDirectoy)).mkdir();
+				if (success) {
+					System.out.println("Directory: " + strDirectoy + " created");
+				}    
+
+			}
 		}
-			    catch (Exception e){//Catch exception if any
-			      System.err.println("Error: " + e.getMessage());
-			    } 
-		
+		catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		} 
+
 	}
 	/*
 	 * Creates directory for each patient
 	 * */
-	
-	public void create_patient_directory(String patientname)
+
+	public boolean create_patient_directory(String patientname)
 	{
 		try{
 			if(!(new File(patientname)).isDirectory())
-		    {
-		    boolean success = (new File("Patients/"+patientname)).mkdir();
-		    if (success) {
-		      System.out.println("Directory: " + patientname + " created");
-		    }  
-		    }
+			{
+				boolean success = (new File("Patients/"+patientname)).mkdir();
+				if (success) {
+					System.out.println("Directory: " + patientname + " created");
+					return true;
+				}  
+			}
 
-		    }catch (Exception e){//Catch exception if any
-		      System.err.println("Error: " + e.getMessage());
-		    } 
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+		return false;
 	}
-	
+
 	/*
 	 * saves sessions in patient's directory as  txt files;
 	 * */
-	
+
 	public void create_session(String patientname, ArrayList<String> file_s)
 	{
 		Iterator<String> iter1 = file_s.iterator();
 		int num_session = new File("Patient/"+patientname).listFiles().length;
 		PrintWriter writer=null;
-		 try {
-		     writer = new PrintWriter(new File("Patients/"+patientname+"/session"+num_session+".txt"));
-		     for (int i = 0; i < file_s.size(); i++) {
-		    	 	String s=iter1.next();
-		    	 	writer.println(s);
-		    	 	//System.out.println(s);
-				}
-		 } catch (FileNotFoundException e) {
-		     System.out.println("File not found");
-		 }
-		 writer.close();
+		try {
+			writer = new PrintWriter(new File("Patients/"+patientname+"/session"+num_session+".txt"));
+			for (int i = 0; i < file_s.size(); i++) {
+				String s=iter1.next();
+				writer.println(s);
+				//System.out.println(s);
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found");
+		}
+		writer.close();
 	}
-	
+
 	/* load previous sessions
 	 * */
-	
+
 	public HashMap<String,ArrayList<String>> load_sessions(String patientname) throws IOException
 	{
 		//ArrayList<ArrayList<String>> array = new ArrayList<ArrayList<String>>();
 		HashMap<String,ArrayList<String>> table = new HashMap<String,ArrayList<String>>();
-		
+
 		int num_session = new File("Patient/"+patientname).listFiles().length;
 		String line ="";
-		
+
 		for(int i=0;i<num_session;i++)
 		{
 			ArrayList<String> array = new ArrayList<String>();
-			 try {
-				 
-		            FileReader fileReader = new FileReader("Patients/"+patientname+"/session"+i+".txt");
+			try {
 
-		            BufferedReader bufferedReader = new BufferedReader(fileReader);
+				FileReader fileReader = new FileReader("Patients/"+patientname+"/session"+i+".txt");
 
-		            while((line = bufferedReader.readLine()) != null) {
-		                System.out.println(line);
-		                array.add(line);
-		            }   
+				BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-		            bufferedReader.close();         
-		        }
-		        catch(FileNotFoundException ex) {
-		            System.out.println("Unable to open file 'session" + i + "'");                
-		        }
+				while((line = bufferedReader.readLine()) != null) {
+					System.out.println(line);
+					array.add(line);
+				}   
+
+				bufferedReader.close();         
+			}
+			catch(FileNotFoundException ex) {
+				System.out.println("Unable to open file 'session" + i + "'");                
+			}
 			table.put("session"+i,array);
 		}
-		
+
 		return table;
 	}
 
